e8748fe290234375f4d385fb5ca734dea1e87b93
Meiyi <hemeiyi0225@gmail.com>
2020-07-05 17:17:16 -0700

    DynamicIntegerArray

--- src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
+++ src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
@@ -2,11 +2,12 @@ package _00_Dynamic_Integer_Array;
 
 public class DynamicIntegerArray {
 	//1. Create a private int array. Don't initialize it.
-
+private int [] array;
 	
 	public DynamicIntegerArray() {
 		//2. Initialize the int array to have 0 elements. 
 		//   This will prevent a null pointer exception
+		array=new int [0];
 	
 	}
 	
@@ -14,21 +15,24 @@ public class DynamicIntegerArray {
 	public void add(int v) {
 		//A. create and initialize a new int array to be one 
 		//   element longer than the member array
-		
+		int [] neww = new int [array.length+1];
 		//B. set the last element of your new array to 
 		//   the value passed into the method
-		
+		neww[neww.length-1]=v;
 		//C. iterate through the member array and 
 		//   copy every element from the member array 
 		//   to the new array
-		
+		for (int i=0;i<array.length;i++) {
+		neww[i]=array[i];
+		}
 		//D. set the member array equal to the new array.
+		array=neww;
 	}
 	
 	//4. Complete the steps in the get method
 	public int get(int location) {
 		//A. Return the value of the memeber array at the location passed in
-		return 0;
+		return array[location];
 	}
 	
 	//5. Run the DynamicArrayTest to see if you are correct so far.
@@ -38,6 +42,7 @@ public class DynamicIntegerArray {
 	public void set(int v, int location) {
 		//A. set the variable at the location passed in to the method
 		//   to the new value v
+		array[location]=v;
 	}
 	
 	//7. Complete the steps in the insert method
