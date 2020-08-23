69f5b4066e6e93c735dac0ea973c9c553c665f6f
Meiyi <hemeiyi0225@gmail.com>
2020-08-23 10:23:57 -0700

    Finished RemovingStuffFromArraylists

--- src/_01_IntroToArrayLists/_04_RemovingStuffFromArrayLists.java
+++ src/_01_IntroToArrayLists/_04_RemovingStuffFromArrayLists.java
@@ -37,11 +37,14 @@ public class _04_RemovingStuffFromArrayLists {
 		System.out.println(stuffIFoundInTheYard.size());
 
 		/* TODO 1: Clean out the dirt but keep the delicious worms. */
-	for (int i=0;i<stuffIFoundInTheYard.size();i++) {
-		for(int ii=0;ii<stuffIFoundInTheYard.get(i).;i++) {
+		
+		for (int i=0;i<stuffIFoundInTheYard.size();i++) {
+			if (stuffIFoundInTheYard.get(i).type.equals("dirt")) {
+				stuffIFoundInTheYard.remove(i);
+				i--;
+			}
 			
 		}
-	}
 		
 		
 		
@@ -81,7 +84,13 @@ public class _04_RemovingStuffFromArrayLists {
 		truth.add('#');
 		/* TODO 2: Remove the hash symbols and print out the truth. */
 
-		
-		
+		for (int i=0;i<truth.size();i++) {
+			if (truth.get(i).equals('#')) {
+				truth.remove(i);
+				i--;
+			}
+			
+		}
+		System.out.println(truth);
 	}
 }
