bab4a9b222d6fb311c36984b4e39b14e1e91b683
Meiyi <hemeiyi0225@gmail.com>
2020-10-18 15:59:59 -0700

    HangMan finished first two

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,5 +1,7 @@
 package _04_HangMan;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Stack;
 
 import javax.swing.JFrame;
@@ -8,12 +10,13 @@ import javax.swing.JOptionPane;
 
 import _03_IntroToStacks._02_TextUndoRedo;
 
-public class HangMan{
+public class HangMan implements KeyListener{
 	public HangMan() {
 	
 run();
 	}
 	Stack<String> words = new Stack<String>();
+	int life=10;
 	JLabel jlabel = new JLabel();
 	JFrame jframe = new JFrame();
 	public static void main(String[] args) {
@@ -21,17 +24,19 @@ run();
 	
 	}
 	
-	
+	String us="";
+	String wrd;
 	public void run(){
 		String nm =JOptionPane.showInputDialog("Enter a number.");
 		int wn = Integer.parseInt(nm);
 		for (int i=0;i<wn;i++) {
 		String randomword = Utilities.readRandomLineFromFile("dictionary.txt");
 		words.push(randomword);
+		words.contains(randomword);
 		}
-	String wrd =words.pop();
+	 wrd =words.pop();
 	System.out.println(wrd);
-	String us="";
+	
 	for (int i=0;i<wrd.length();i++) {
 	us=us+"_ ";
 	}
@@ -39,6 +44,41 @@ run();
 		jframe.setVisible(true);
 		jlabel.setText(us);
 		jframe.pack();
+		jframe.addKeyListener(this);
+	}
+
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		char letter = e.getKeyChar();
+		boolean foundmatch=false;
+		for (int i=0;i<wrd.length();i++) {
+			if (letter==wrd.charAt(i)) {
+				String cc=us.substring(0,i*2)+letter+us.substring(i*2+1);
+				us=cc;
+				foundmatch=true;
+				//the thing on left side is receiving the value.//
+			}
+		}
+		if (foundmatch==false) {
+			life--;
+		}
+		
+	}
+
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
