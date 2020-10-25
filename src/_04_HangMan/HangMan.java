6ec91ed32672fd9d9e2f7659cb23d3245b884a7a
Meiyi <hemeiyi0225@gmail.com>
2020-10-25 15:54:53 -0700

    HangMan finished

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -27,44 +27,69 @@ run();
 	String us="";
 	String wrd;
 	public void run(){
-		String nm =JOptionPane.showInputDialog("Enter a number.");
-		int wn = Integer.parseInt(nm);
-		for (int i=0;i<wn;i++) {
-		String randomword = Utilities.readRandomLineFromFile("dictionary.txt");
-		words.push(randomword);
-		words.contains(randomword);
-		}
-	 wrd =words.pop();
-	System.out.println(wrd);
-	
-	for (int i=0;i<wrd.length();i++) {
-	us=us+"_ ";
-	}
+		starG();
 		jframe.add(jlabel);
 		jframe.setVisible(true);
+		jframe.addKeyListener(this);
+		newWord();
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+public void starG () {
+	String nm =JOptionPane.showInputDialog("Enter a number.");
+	int wn = Integer.parseInt(nm);
+	for (int i=0;i<wn;i++) {
+	String randomword = Utilities.readRandomLineFromFile("dictionary.txt");
+	words.push(randomword);
+	words.contains(randomword);
+	}
+}
+public void newWord() {
+	 wrd =words.pop();
+		System.out.println(wrd);
+		us="";
+		for (int i=0;i<wrd.length();i++) {
+		us=us+"_ ";
+		}
 		jlabel.setText(us);
 		jframe.pack();
-		jframe.addKeyListener(this);
-	}
-
-
+		life=10;
+}
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 		char letter = e.getKeyChar();
+		System.out.println(letter);
 		boolean foundmatch=false;
 		for (int i=0;i<wrd.length();i++) {
 			if (letter==wrd.charAt(i)) {
 				String cc=us.substring(0,i*2)+letter+us.substring(i*2+1);
 				us=cc;
+				jlabel.setText(cc);
+				jframe.pack();
 				foundmatch=true;
 				//the thing on left side is receiving the value.//
 			}
 		}
+		
 		if (foundmatch==false) {
 			life--;
 		}
+		if (!jlabel.getText().contains("_")) {
+			newWord();
+				}
 		
+		
+		if (life==0) {
+			JOptionPane.showMessageDialog(null, "Game Over");
+			String usec=JOptionPane.showInputDialog("Do you want to play again? Please enter YES or NO.");
+			if (usec.equalsIgnoreCase("YES")) {
+				starG();
+				newWord();
+				jframe.pack();
+			}
+		}
+		 
+	
 	}
 
 
